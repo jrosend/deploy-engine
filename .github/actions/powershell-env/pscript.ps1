ca1f67c77e8d003b2c66e73fece678d085700861
jrosend <jhonatas@hotmail.com>
2022-01-13 11:56:24 -0300

    Testing boolean expression

--- .github/actions/powershell-env/pscript.ps1
+++ .github/actions/powershell-env/pscript.ps1
@@ -2,4 +2,5 @@ Write-Output "########################################"
 
 Write-Output "########################################"
 Write-Output ${env:TARGET_ENV}
+Write-Output ${env:SHOULD_START_TRIGGERS}
 Write-Output "########################################"
