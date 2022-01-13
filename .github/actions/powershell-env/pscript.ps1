b812bbc1c6eb99ce3605e275e46016b74b991151
jrosend <jhonatas@hotmail.com>
2022-01-13 16:33:26 -0300

    Passing action path to script

--- .github/actions/powershell-env/pscript.ps1
+++ .github/actions/powershell-env/pscript.ps1
@@ -3,4 +3,5 @@ Write-Output "########################################"
 Write-Output "########################################"
 Write-Output ${env:TARGET_ENV}
 Write-Output ${env:SHOULD_START_TRIGGERS}
+Write-Output ${env:ACTION_PATH}
 Write-Output "########################################"
