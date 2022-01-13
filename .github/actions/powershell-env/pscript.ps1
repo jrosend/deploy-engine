c2d1c36fe3c5ba4a7b8582857a4dba9fe1c45df9
jrosend <jhonatas@hotmail.com>
2022-01-13 16:39:57 -0300

    .

--- .github/actions/powershell-env/pscript.ps1
+++ .github/actions/powershell-env/pscript.ps1
@@ -4,6 +4,7 @@ Write-Output "########################################"
 Write-Output ${env:TARGET_ENV}
 Write-Output ${env:SHOULD_START_TRIGGERS}
 Write-Output ${env:ACTION_PATH}
+Write-Output ${github:action_path}
 Write-Output "########################################"
 
 & ${env:ACTION_PATH}/pscript2.ps1
